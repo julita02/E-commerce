6c3c24564631e5b23dee0a802ee2ec803c874e3c
julita02 <julitaspahiu@gmail.com>
2022-02-02 16:31:51 +0100

    Sign Up with Email and Password

--- src/App.js
+++ src/App.js
@@ -32,16 +32,15 @@ class App extends React.Component {
 
        userRef.onSnapshot(snapShot=> {
          this.setState({
-
           currentUser:{
             id:snapShot.id,
             ...snapShot.data() 
-        }
-        
+        }        
          });
+         console.log(this.state);
        });       
      }
-     
+
      this.setState({ currentUser:userAuth});
     });  
     
