661131ac3d98d525ea9545e558c92a8162901d18
julita02 <julitaspahiu@gmail.com>
2022-02-02 15:35:48 +0100

    Storing User Data

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Header from './components/header/header.component.jsx';
 
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign.component';
 
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 
@@ -26,11 +26,29 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth =auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user});
-      // console.log(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+     if(userAuth) {
+       const userRef = await createUserProfileDocument(userAuth);
+
+       userRef.onSnapshot(snapShot=> {
+         this.setState({
+
+          currentUser:{
+            id:snapShot.id,
+            ...snapShot.data() 
+        }
+        
+         });
+       });       
+     }
+     
+     this.setState({ currentUser:userAuth});
+    });  
+    
   }
+
+  
+
   componentWillUnmount(){
     this.unsubscribeFromAuth();
   }
