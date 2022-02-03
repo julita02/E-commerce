9a108f2e36856851670015d8d6dfc784dfefe370
julita02 <julitaspahiu@gmail.com>
2022-02-03 11:15:20 +0100

    Added Redux

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import { Switch,Route} from "react-router-dom";
+import {connect} from 'react-redux';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -12,41 +13,36 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign.component';
 
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
+import {setCurrentUser} from './redux/user/user.actions';
 
 
 class App extends React.Component {
 
-  constructor(){
-    super();
-    this.state={
-      currentUser:null
-    }
-  }
+  
 
   unsubscribeFromAuth = null;
 
   componentDidMount(){
+    
+const {setCurrentUser} =this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
      if(userAuth) {
        const userRef = await createUserProfileDocument(userAuth);
 
        userRef.onSnapshot(snapShot=> {
-         this.setState({
-          currentUser:{
+        setCurrentUser({
             id:snapShot.id,
-            ...snapShot.data() 
-        }        
-         });
-         console.log(this.state);
+            ...snapShot.data()               
+         });         
+        //  console.log(this.state);
        });       
      }
 
      this.setState({ currentUser:userAuth});
-    });  
+    });
     
-  }
-
-  
+  } 
 
   componentWillUnmount(){
     this.unsubscribeFromAuth();
@@ -56,7 +52,7 @@ class App extends React.Component {
   return (
     <div >
 
-<Header currentUser = {this.state.currentUser}/>
+<Header />
    
 
       <Switch>
@@ -70,5 +66,12 @@ class App extends React.Component {
   );
 }
 }
+const mapDispatchToProps = dispatch => ({
 
-export default App;
+  setCurrentUser:user => dispatch(setCurrentUser(user))
+
+
+})
+export default connect(
+  null,
+  mapDispatchToProps) (App);
