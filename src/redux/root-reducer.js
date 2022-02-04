7699905ad0546b0793de0366ff5d67e91b813e91
julita02 <julitaspahiu@gmail.com>
2022-02-04 16:16:21 +0100

    Directory State into Redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,24 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
-  user: userReducer,
-  cart: cartReducer
+import directoryReducer from './directory/directory.reducer';
 
+// object with a key from the root , passing to storage the whitelist property (an array containing the string names of a reducer that we want to store)
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+  directory:directoryReducer
 });
+
+
+export default persistReducer(persistConfig, rootReducer);
