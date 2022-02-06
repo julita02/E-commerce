e6145d083c580796139043f67453467133d795c9
julita02 <julitaspahiu@gmail.com>
2022-02-06 01:26:13 +0100

    Updated corresponding components with new page flow

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,10 +4,13 @@ import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-const  ShopPage  =() =>( 
-  
+import CollectionPage from '../collection/collection.component';
+const  ShopPage  =({match}) =>( 
+
     <div className='shop-page'>    
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
      </div>
      
      );
